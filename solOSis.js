e4b8905836e955270b69822d516eb56ec91afe0b
julietasegovia <julietavsegovia@gmail.com>
2026-03-31 15:12:27 -0300

    Added window header + topbar features
    Now the window is draggable, closable by clicking on the X on the header, and reopenable by clicking the top-left solosis on the top bar

--- solOSis.js
+++ solOSis.js
@@ -6,3 +6,66 @@ function updateTime(){
 }
 
 setInterval(updateTime, 1000);
+
+function dragElement(element) {
+  var initialX = 0;
+  var initialY = 0;
+  var currentX = 0;
+  var currentY = 0;
+
+  if (document.getElementById(element.id + "header")) {
+    document.getElementById(element.id + "header").onmousedown = startDragging;
+  } else {
+    element.onmousedown = startDragging;
+  }
+
+  function startDragging(e) {
+    e = e || window.event;
+    e.preventDefault();
+    initialX = e.clientX;
+    initialY = e.clientY;
+    document.onmouseup = stopDragging;
+    document.onmousemove = dragElement;
+  }
+
+  function dragElement(e) {
+    e = e || window.event;
+    e.preventDefault();
+    currentX = initialX - e.clientX;
+    currentY = initialY - e.clientY;
+    initialX = e.clientX;
+    initialY = e.clientY;
+    element.style.top = (element.offsetTop - currentY) + "px";
+    element.style.left = (element.offsetLeft - currentX) + "px";
+  }
+
+  function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+function closeWindow(element){
+    element.style.display = "none";
+}
+
+function openWindow(element){
+    element.style.display = "block";
+    element.style.top = "";
+    element.style.left = "";
+    element.style.transform = "translate(-50%, -50%)";
+}
+
+var welcomeWindow = document.getElementById("welcome-page");
+var welcomeClose = document.querySelector("#welcome-pageclose");
+var welcomeOpen = document.querySelector("#solosis-come-back");
+
+dragElement(welcomeWindow);
+
+welcomeClose.addEventListener("click", () => {
+    closeWindow(welcomeWindow);
+});
+
+welcomeOpen.addEventListener("click", () => {
+    openWindow(welcomeWindow);
+});
